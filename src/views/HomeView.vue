77e3ccaf93a014ce7b28249898c4ad2365293288
ISedykh <isedykh@sportmasterlab.net>
2024-07-03 12:04:33 +0300

    доработал главную

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
 <template>
-  <Header></Header>
+  <Header title="Доска заметок">
+    <nav>
+      <!--        <RouterLink to="/">Home</RouterLink>-->
+      <RouterLink :to="`/add`">Добавить заметку</RouterLink>
+      <!--        <RouterLink to="/view">View</RouterLink>
+      <RouterLink to="/edit/123">Edit</RouterLink>-->
+    </nav>
+  </Header>
+
+  <div class="wrapper">
+    <NotesList :notes="notesStore.notes" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
+import { RouterLink } from 'vue-router';
+import NotesList from '@/components/notes/NotesList.vue';
+
+import { useNotesStore } from '@/stores/notes';
+
+const notesStore = useNotesStore();
 </script>
