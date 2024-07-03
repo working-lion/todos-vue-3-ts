15ad0c6c1b9856b20480bd7efc109d3101bd5083
ISedykh <isedykh@sportmasterlab.net>
2024-07-03 12:04:14 +0300

    доработал heder

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <template>
-  <header></header>
+  <header :title="title">
+    <title>{{ title }}</title>
+
+    <slot />
+  </header>
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style>
+header {
+  padding-bottom: 30px;
+}
+</style>
