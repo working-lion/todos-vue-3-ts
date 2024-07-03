d3600f845b68caa78cc708840a4f14b376140e15
ISedykh <isedykh@sportmasterlab.net>
2024-07-03 11:59:10 +0300

    доработал инпуты

---
+++ src/components/ui/inputs/Checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <input
+    :id="id"
+    type="checkbox"
+    v-model="checked"
+  />
+  <label :for="id"></label>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+interface Props {
+  modelValue?: boolean;
+  id: string;
+  label?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: false,
+  id: '',
+  label: '',
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const checked = ref(props.modelValue);
+
+const onChange = (checked: boolean) => {
+  emit('update:modelValue', checked);
+};
+
+watch(checked, onChange);
+</script>
---
+++ src/components/ui/inputs/Input.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="input-wrap">
+    <label
+      class="input__label"
+      :for="id"
+      >{{ label }}</label
+    >
+    <input
+      :id="id"
+      class="input"
+      type="text"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="onInput"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue?: string | number;
+  id: string;
+  label?: string;
+  placeholder?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  modelValue: '',
+  id: '',
+  label: '',
+  placeholder: '',
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+
+  emit('update:modelValue', target.value);
+};
+</script>
+
+<style>
+.input-wrap {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  height: 25px;
+  border-radius: 8px;
+  outline: none;
+  padding: 0 7px;
+  font-size: 16px;
+  color: gray;
+}
+
+.input__label {
+  font-size: 14px;
+  display: block;
+}
+</style>
