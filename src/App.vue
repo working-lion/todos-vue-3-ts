65a06b0a8b0db8859d665c79a5a8e80149267092
ISedykh <isedykh@sportmasterlab.net>
2024-07-03 11:56:15 +0300

    добавил app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <!--        <RouterLink to="/">Home</RouterLink>-->
-        <RouterLink :to="`/add/:${newNoteId}`">Add</RouterLink>
-        <!--        <RouterLink to="/view">View</RouterLink>
-        <RouterLink to="/edit/123">Edit</RouterLink>-->
-      </nav>
-    </div>
-  </header>
-
   <main>
     <RouterView />
   </main>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-
-import { useNotesStore } from '@/stores/notes';
-
-const notesStore = useNotesStore();
-
-const newNoteId = notesStore.getCount();
+import { RouterView } from 'vue-router';
 </script>
-
-<style scoped></style>
