4338d5156b06a879163e015ba4bf83e920ddf1d8
ISedykh <isedykh@sportmasterlab.net>
2024-07-04 19:31:23 +0300

    доработал страницу 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>Not Found!</div>
+  <div class="not-found-title">Страница не найдена...</div>
+  <p><LinkHome /></p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LinkHome from '@/components/links/LinkHome.vue';
+</script>
 
-<style scoped></style>
+<style>
+.not-found-title {
+  margin-bottom: 25px;
+}
+</style>
