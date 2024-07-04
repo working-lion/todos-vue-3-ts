6be980dd281bb0f410dc4952f1141103c926e880
ISedykh <isedykh@sportmasterlab.net>
2024-07-04 19:30:03 +0300

    добавил компонент ссылки на редактирование заметки

---
+++ src/components/links/LinkEdit.vue
@@ -0,0 +1,12 @@
+<template>
+  <VLink :link>Редактировать</VLink>
+</template>
+
+<script setup lang="ts">
+import VLink from '@/components/ui/VLink.vue';
+import { computed } from 'vue';
+
+const props = defineProps<{ noteId: number }>();
+
+const link = computed(() => `/note/${props.noteId}`);
+</script>
