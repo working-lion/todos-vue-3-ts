1a3d86d6413f4265a0ba7cc467e4c1db6ffe126a
ISedykh <isedykh@sportmasterlab.net>
2024-07-04 19:27:39 +0300

    создал компонент кнопки удаления

---
+++ src/components/buttons/ButtonDelete.vue
@@ -0,0 +1,29 @@
+<template>
+  <VButton @click="remove">Удалить</VButton>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import { useNotesStore } from '@/stores/notes';
+
+import VButton from '@/components/ui/VButton.vue';
+
+interface Props {
+  noteId: number;
+  toHome?: boolean;
+}
+
+const notesStore = useNotesStore();
+
+const props = withDefaults(defineProps<Props>(), {
+  toHome: true,
+});
+
+const remove = () => {
+  notesStore.remove(props.noteId);
+
+  if (props.toHome) {
+    router.push({ name: 'home' });
+  }
+};
+</script>
